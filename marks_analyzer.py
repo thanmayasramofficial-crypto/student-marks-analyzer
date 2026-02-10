d5a6a179c42002861bb1f26d979a6fcb3462b9ec
thanmayasramofficial-crypto <thanmayasramofficial@gmail.com>
2026-02-10 12:42:28 +0530

    Add grade logic to marks analyzer
    Added grade calculation based on average marks.

--- marks_analyzer.py
+++ marks_analyzer.py
@@ -1,4 +1,4 @@
-# Student Marks Analyzer
+# Student Marks Analyzer with Grade
 
 name = input("Enter student name: ")
 
@@ -9,6 +9,18 @@ mark3 = int(input("Enter mark 3: "))
 total = mark1 + mark2 + mark3
 average = total / 3
 
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
 print("\nStudent Name:", name)
 print("Total Marks:", total)
-print("Average Marks:", average)
+print("Average:", average)
+print("Grade:", grade)
